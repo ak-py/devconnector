65c6b8379f4600e9abc4e41d50b551cb5b05ca2b
Akshit Gupta <akshitgupta1996@gmail.com>
2019-02-26 18:15:44 -0800

    register validations completed

--- routes/validation/register.js
+++ routes/validation/register.js
@@ -4,8 +4,41 @@ const isEmpty = require("./is-empty");
 module.exports = function validateRegisterInput(data) {
   let errors = {};
 
+  // Force empty input to be an empty string
+  data.name = !isEmpty(data.name) ? data.name : "";
+  data.email = !isEmpty(data.email) ? data.email : "";
+  data.password = !isEmpty(data.password) ? data.password : "";
+  data.password2 = !isEmpty(data.password2) ? data.password2 : "";
+
+  // Name
   if (!Validator.isLength(data.name, { min: 2, max: 30 })) {
     errors.name = "Name must be between 2 and 30 characters";
+  }
+  if (Validator.isEmpty(data.name)) {
+    errors.name = "Name is field is required";
+  }
+
+  // Email
+  if (Validator.isEmpty(data.email)) {
+    errors.email = "Email is field is required";
+  }
+  if (!Validator.isEmail(data.email)) {
+    errors.email = "Email is invalid";
+  }
+
+  // Password
+  if (Validator.isEmpty(data.password)) {
+    errors.password = "Password is field is required";
+  }
+  if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
+    errors.password = "Password must be between 6 and 30 characters";
+  }
+
+  // Confirm Password
+  if (Validator.isEmpty(data.password2)) {
+    errors.password2 = "Confirm Password is field is required";
+  } else if (!Validator.equals(data.password, data.password2)) {
+    errors.password2 = "Passwords must match";
   }
 
   return {
