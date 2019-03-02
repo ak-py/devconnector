8359a2c65d7ae708cb3f70acc918616ff972e1fa
Akshit Gupta <akshitgupta1996@gmail.com>
2019-03-01 22:13:10 -0800

    POST request added for creating a post

---
+++ validation/post.js
@@ -0,0 +1,20 @@
+const Validator = require("validator");
+const isEmpty = require("./is-empty");
+
+module.exports = function validatePostInput(data) {
+  let errors = {};
+
+  // Force empty input to be an empty string
+  data.text = !isEmpty(data.text) ? data.text : "";
+
+  // Text
+  if (Validator.isEmpty(data.text)) {
+    errors.text = "Text field is required";
+  } else if (!Validator.isLength(data.text, { min: 10, max: 300 })) {
+    errors.text = "Post must be between 10 and 300 characters";
+  }
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
