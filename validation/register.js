723d48be9173eb0cf475262cc6afed47bfacd056
Akshit Gupta <akshitgupta1996@gmail.com>
2019-03-02 13:32:19 -0800

    validation added to the register form from backend

--- validation/register.js
+++ validation/register.js
@@ -21,16 +21,14 @@ module.exports = function validateRegisterInput(data) {
   // Email
   if (Validator.isEmpty(data.email)) {
     errors.email = "Email field is required";
-  }
-  if (!Validator.isEmail(data.email)) {
+  } else if (!Validator.isEmail(data.email)) {
     errors.email = "Email is invalid";
   }
 
   // Password
   if (Validator.isEmpty(data.password)) {
     errors.password = "Password field is required";
-  }
-  if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
+  } else if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
     errors.password = "Password must be between 6 and 30 characters";
   }
 
