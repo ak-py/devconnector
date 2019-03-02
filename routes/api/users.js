a68aaad2347a354640378901696af3d6fcc2f812
Akshit Gupta <akshitgupta1996@gmail.com>
2019-03-01 21:39:48 -0800

    fixed typo api users files

--- routes/api/users.js
+++ routes/api/users.js
@@ -53,7 +53,8 @@ router.post("/register", (req, res) => {
           newUser.password = hash;
           newUser
             .save()
-            .then(user => res.json(user).catch(err => console.log(err)));
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
         });
       });
     }
