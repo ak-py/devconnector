7e8e0f64fff29b371a0432bfa21a2c218a3809f3
Akshit Gupta <akshitgupta1996@gmail.com>
2019-03-01 16:03:13 -0800

    added valdiations for profile & moved validations directory outside api

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const passport = require("passport");
 
+// Load Validation
+const validateProfileInput = require("../../validation/profile");
+
 // Load Profile
 const Profile = require("../../models/Profile");
 // Load User Profile
@@ -41,6 +44,14 @@ router.post(
   "/",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
+    const { errors, isValid } = validateProfileInput(req.body);
+
+    // Check Validation
+    if (!isValid) {
+      // Return any errors with 400 status
+      return res.status(400).json(errors);
+    }
+
     // Get feilds
     const profileFeilds = {};
     profileFeilds.user = req.user.id;
