58b4264bdf5baf0402c76c8e4fd02836148e5b06
Akshit Gupta <akshitgupta1996@gmail.com>
2019-03-07 23:18:54 -0800

    added post feed component, need to add post item

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -25,3 +25,29 @@ export const addPost = postData => dispatch => {
       })
     );
 };
+
+// Get Posts
+export const getPosts = () => dispatch => {
+  dispatch(setPostLoading);
+  axios
+    .get("/api/posts")
+    .then(res =>
+      dispatch({
+        type: GET_POSTS,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_POSTS,
+        payload: null
+      })
+    );
+};
+
+// Set loading state
+export const setPostLoading = () => {
+  return {
+    type: POST_LOADING
+  };
+};
