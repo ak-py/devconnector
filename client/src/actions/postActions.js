fe07921a5c04a05b654837ef843aa36bc948e3b3
Akshit Gupta <akshitgupta1996@gmail.com>
2019-03-07 21:55:35 -0800

    added Not Found

---
+++ client/src/actions/postActions.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+import {
+  GET_POST,
+  GET_POSTS,
+  POST_LOADING,
+  DELETE_POST,
+  GET_ERRORS,
+  ADD_POST
+} from "./types";
+
+// Add Post
+export const addPost = postData => dispatch => {};
