8a11ceae6fb87c81a76b3f1a9e38d126b5f6a105
Akshit Gupta <akshitgupta1996@gmail.com>
2019-03-07 21:57:24 -0800

    implemented addPost in post Actions

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -9,4 +9,19 @@ import {
 } from "./types";
 
 // Add Post
-export const addPost = postData => dispatch => {};
+export const addPost = postData => dispatch => {
+  axios
+    .post("/api/posts", postData)
+    .then(res =>
+      dispatch({
+        type: ADD_POST,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
